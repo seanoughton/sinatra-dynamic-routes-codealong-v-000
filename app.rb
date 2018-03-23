26012c0684b5f4e0db7e89a7503978fb4fc60559
Sean Oughton <seanoughton@gmail.com>
2018-03-23 19:50:12 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -22,7 +22,8 @@ class App < Sinatra::Base
   get "/goodbye/:num/1:num2" do
     @num1 = params[:num1]
     @num2 = params[:num2]
-    "<%= @num1+@num2 %>""
+    "#{@num1+@num2}"
+    #"<%= @num1+@num2 %>""
   end
 
 
